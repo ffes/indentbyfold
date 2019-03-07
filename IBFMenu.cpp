14d1ecd13e3230bdd68ff04e0b6ff43b4843b400
Frank Fesevur <ffes@users.sourceforge.net>
2019-03-07 08:29:02 +0100

    Add menu item to open the online manual

--- IBFMenu.cpp
+++ IBFMenu.cpp
@@ -8,12 +8,19 @@ extern IBFPlugin ibfplugin;
 FuncItem IBFMenu::arrFuncItems[N_NBFUNCITEMS] = {
 	{ L"Reindent File", reIndentFile, 0, false, NULL },
 	{ L"", NULL, 0, false, NULL }, // separator
+	{ L"Open Online Manual", openOnlineManual, 0, false, NULL },
+	{ L"", NULL, 0, false, NULL }, // separator
 	{ L"About", aboutDlg, 0, false, NULL }
 };
 
 void IBFMenu::reIndentFile()
 {
 	ibfplugin.reindentFile();
+}
+
+void IBFMenu::openOnlineManual()
+{
+	ShellExecute(NULL, L"open", L"https://indentbyfold.readthedocs.io", NULL, NULL, SW_SHOWNORMAL);
 }
 
 void IBFMenu::aboutDlg()
--- IBFMenu.h
+++ IBFMenu.h
@@ -8,8 +8,10 @@ class IBFMenu : public CNppPluginMenu
 {
 public:
 	enum NMenuItems {
-	    N_REINDENTFILE = 0,
+	    N_REINDENT_FILE = 0,
 	    N_SEPARATOR1,
+		N_OPEN_INLINE_MANUAL,
+	    N_SEPARATOR2,
 	    N_ABOUT,
 
 	    N_NBFUNCITEMS
@@ -18,6 +20,7 @@ public:
 
 public:
 	static void		reIndentFile();
+	static void		openOnlineManual();
 	static void		aboutDlg();
 };
 //---------------------------------------------------------------------------
